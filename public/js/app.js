88caac399afdd3a5bca2e7bd36d767afe3b08d08
huynhieulh <hieub1609520@gmail.com>
2024-03-22 23:25:46 +0700

    Update new style, add modal import Items

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,19 @@
 (function ($) {
     "use strict";
 
+    Inputmask.extendAliases({
+        vnd: {
+            suffix: " đ", 
+            groupSeparator: ".",
+            alias: "numeric",
+            placeholder: "0",
+            autoGroup: true,
+            digits: 0,
+            digitsOptional: true,
+            clearMaskOnLostFocus: false
+        }
+    });
+
     $(document).on("click",".menu .menu-item", function() {
         $(".menu .menu-item.active").removeClass("active")
         $(this).addClass("active")
@@ -16,6 +29,10 @@
         // Show container
         $(".content .content-tab.active").removeClass("active")
         $("#" + key).addClass("active");
+        // Auto close menu tab if mobile
+        if (Math.min(window.screen.width, window.screen.height) < 768 || navigator.userAgent.indexOf("Mobi") > -1){
+            $(".sidebar-toggler").trigger("click")
+        }
     })
 
 })(jQuery);
